aa9fc93eec56c7d2e326c292678bf61f19da3efe
Sarang Gunjal <sarang@clayfin.com>
2022-05-29 21:11:17 +0530

    ...

--- snake-ladder-game/src/components/Game.jsx
+++ snake-ladder-game/src/components/Game.jsx
@@ -4,13 +4,24 @@ import { Main } from './/Main';
 
 function Game() 
 { 
-  const [count,setCount]  = useState(0);  
-  const [place,setPlace] = useState(1)
+  const [count1,setCount1]  = useState(0);
+  const [count2,setCount2]  = useState(0);  
+  const [place,setPlace] = useState(1);
+  const [place1,setPlace1] = useState(1);
+  
+  const [flag,setFlag] = useState(true);
+  
+  
+  const user1 = "Srng";
+  const user2 = "Swti";
 
+  const [diceuser,setDiceUser] = useState(user1);
+  
   useEffect(
       ()=>{
-        setPlace(count+place);
-      },[count]
+        setPlace(count1+place);
+        console.log(flag);
+      },[count1,flag]
     );
 
   if(place >= 100)
@@ -18,14 +29,19 @@ function Game()
     alert("Win => 👑");    
     setPlace(1);    
   }
-    
   
 
   const handledice = () =>
   {
-    setCount(Math.floor(Math.random() * (6 - 1 + 1) + 1));
-    //setPlace(place + count);
- 
+    diceuser==user1 ? setDiceUser(user2) : setDiceUser(user1)
+    setFlag(flag===true ? false : true);
+
+
+
+    setCount1(Math.floor(Math.random() * (6 - 1 + 1) + 1));
+    //setPlace(place + count1);
+    
+
     if(place === 69)
     {
       setPlace(88);
@@ -54,6 +70,35 @@ function Game()
     {
       setPlace(1);
     }
+    //==================================================
+    if(place1 === 69)
+    {
+      setPlace1(88);
+    }    
+    else if(place1 === 67)
+    {
+      setPlace1(48);
+    }
+    else if(place1 === 14)
+    {
+      setPlace1(54);
+    }
+    else if(place1 === 42)
+    {
+      setPlace1(11);
+    }
+    else if(place1 === 72)
+    {
+      setPlace1(28);
+    }    
+    else if(place1 === 28)
+    {
+      setPlace1(72);
+    }
+    else if(place1 === 89 || place1===46 || place1===97 )
+    {
+      setPlace1(1);
+    }
 
 
   }
@@ -67,13 +112,13 @@ function Game()
 
       <div className='button'>
         <button onClick={() => handledice()}>
-            Dice
+            {diceuser}
         </button>
         <br />
         <br />
         <br />
           <div className='count'>
-            <h1>{count}</h1>
+            <h1>{count1}</h1>
           </div>       
       </div>
     </div>
