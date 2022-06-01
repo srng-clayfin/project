9f8774599706fca7e55c34b37c8a1f1bf5e273ff
Sarang Gunjal <sarang@clayfin.com>
2022-06-01 12:50:51 +0530

    ....

--- snake-ladder-game/src/components/Game.jsx
+++ snake-ladder-game/src/components/Game.jsx
@@ -5,7 +5,8 @@ import { userContext } from "../App";
 import { useNavigate } from 'react-router-dom';
 
 function Game(p) 
-{     
+{
+
   const {p1,setP1,p2,setP2} = useContext(userContext) 
   const nav = useNavigate()
   
@@ -42,22 +43,32 @@ function Game(p)
   }
 
     useEffect(
-      ()=>{   
+      ()=>{
         
-        if(count1!=0)
+        
+        if(place1 >= 100)
         {
-          setImg(count1);  
+          playerWin(user2);
+        }
+        else if(place2 >= 100)
+        {
+          playerWin(user1);
         }
 
-        if(diceuser == user1)
-        {
-          setPlace1(count1+place1);  
-        }
-        else
-        {
-          setPlace2(count1+place2);
-        }        
+              if(count1!=0)
+              {
+                setImg(count1);  
+              }
 
+              if(diceuser == user1)
+              {
+                setPlace1(count1+place1);  
+              }
+              else
+              {
+                setPlace2(count1+place2);
+              }
+        
       },[flag]
     );
     
@@ -81,10 +92,11 @@ function Game(p)
 
       },[repeat]
     )
-
-
+  
+  const [dnm, setDnm] = useState(false)
   const handledice = () =>
   {
+      setDnm(true)
       diceuser==user2 ? setDiceUser(user1) : setDiceUser(user2);
       setFlag(flag===true ? false : true);    
       setPrevious(count1);
@@ -123,35 +135,34 @@ function Game(p)
         setPlace1(1);
       }
 
-      
-      if(place2 === 69)
-      {
-        setPlace2(88);
-      }    
-      else if(place2 === 67)
-      {
-        setPlace2(48);
-      }
-      else if(place2 === 14)
-      {
-        setPlace2(54);
-      }
-      else if(place2 === 42)
-      {
-        setPlace2(11);
-      }
-      else if(place2 === 72)
-      {
-        setPlace2(28);
-      }    
-      else if(place2 === 28)
-      {
-        setPlace2(72);
-      }
-      else if(place2 === 89 || place2 ===46 || place2 ===97 )
-      {
-        setPlace2(1);
-      }
+            if(place2 === 69)
+            {
+              setPlace2(88);
+            }    
+            else if(place2 === 67)
+            {
+              setPlace2(48);
+            }
+            else if(place2 === 14)
+            {
+              setPlace2(54);
+            }
+            else if(place2 === 42)
+            {
+              setPlace2(11);
+            }
+            else if(place2 === 72)
+            {
+              setPlace2(28);
+            }    
+            else if(place2 === 28)
+            {
+              setPlace2(72);
+            }
+            else if(place2 === 89 || place2 ===46 || place2 ===97 )
+            {
+              setPlace2(1);
+            }
   }
 
   const previousPlace = () =>
@@ -195,6 +206,7 @@ function Game(p)
         <br />
         <br />       
           <h3>
+            {dnm ? diceuser == user1 ? user2+" points" : user1+" points" : null} 
           </h3>
           <div>
             <img src={diceImage} />
